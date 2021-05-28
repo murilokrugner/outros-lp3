295e082c6f2fd9613e45775b577471a7c08048eb
murilokrugner <murilokrugner@hotmail.com>
2021-05-28 11:24:34 -0300

    finished

---
+++ src/pages/Home.tsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import {Alert} from 'react-native';
+import { Header } from '../components/Header';
+import { MyTasksList } from '../components/MyTasksList';
+import { TodoInput } from '../components/TodoInput';
+
+interface Task {
+  id: number;
+  title: string;
+  done: boolean;
+}
+
+export function Home() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  function handleAddTask(newTaskTitle: string) {
+    //TODO - add new task if it's not empty
+    if(newTaskTitle === '') {
+      Alert.alert('Digite a sua tarefa para adicioná-la');
+      return;
+    }
+
+    const data = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false,
+    }
+
+    setTasks(oldState => [...oldState, data]);
+  }
+
+  function handleMarkTaskAsDone(id: number) {
+    //TODO - mark task as done if exists
+    tasks.map(item => {
+      if(item.id === id) {
+        item.done = item.done === true ? false : true;
+      }      
+    });
+
+    let newState = tasks;
+
+    setTasks([...newState]);
+  }
+
+  function handleRemoveTask(id: number) {
+    //TODO - remove task from state
+    setTasks(oldState => oldState.filter(
+      task => task.id !== id
+    ));
+  }
+
+  return (
+    <>
+      <Header />
+
+      <TodoInput addTask={handleAddTask} />
+
+      <MyTasksList 
+        tasks={tasks} 
+        onPress={handleMarkTaskAsDone} 
+        onLongPress={handleRemoveTask} 
+      />
+    </>
+  )
+}
